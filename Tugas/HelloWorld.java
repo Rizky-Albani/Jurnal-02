c05602e03e8ddb0591c3882e8697e45f72180bd5
Rizky-Albani <albanivenom123@gmail.com>
2021-09-27 16:01:15 +0700

    HelloWorld.java

---
+++ Tugas/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Hello Rizky!") ;
+		System.out.println("Nama : Rizky Albani") ;
+		System.out.println("NIM : 6706213051!") ;
+		System.out.println("Kelas : 4503") ;
+	}
+}
